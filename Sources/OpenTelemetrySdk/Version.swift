58b78afef8d092e28715ed3a3081095465caf702
bryce-b <bryce-b@users.noreply.github.com>
2024-09-30 23:44:17 +0000

    version bump to 101

--- Sources/OpenTelemetrySdk/Version.swift
+++ Sources/OpenTelemetrySdk/Version.swift
@@ -1,10 +1,1 @@
-//
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-// 
-
-import Foundation
-
-extension Resource {
-    public static let OTEL_SWIFT_SDK_VERSION : String = "1.9.2"
-}
+//\n// Copyright The OpenTelemetry Authors\n// SPDX-License-Identifier: Apache-2.0\n//\n\nimport Foundation\n\nextension Resource {\n\tpublic static let OTEL_SWIFT_SDK_VERSION : String = "101"\n}\n
